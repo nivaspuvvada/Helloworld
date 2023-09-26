fe62ad5f6cc2de7ce9a5a27238d3bb3eb62e51f0
nivaspuvvada <nivaspuvvada1126@gmail.com>
2023-09-26 12:02:07 -0500

    Hello

---
+++ Demoproject/src/main/java/com/example/Demoproject/Helloworld.java
@@ -0,0 +1,12 @@
+package com.example.Demoproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworld {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello Nivas";
+    }
+}
