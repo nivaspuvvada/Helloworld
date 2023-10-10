dff67f6d7b24886939760c2f20b5bb99b2fec20c
nivaspuvvada <nivaspuvvada1126@gmail.com>
2023-10-10 16:39:39 -0500

    multiplierusingforloop

---
+++ Demoproject/src/main/java/com/example/Demoproject/multiplierusingforloop.java
@@ -0,0 +1,10 @@
+package com.example.Demoproject;
+
+public class multiplierusingforloop {
+    public static void main(String[] args){
+        int num=6;
+        for (int mul=1;mul<8;mul++){
+            System.out.printf("%d*%d=%d \n",num,mul,num*mul);
+        }
+    }
+}
