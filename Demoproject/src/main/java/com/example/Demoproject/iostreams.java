da28153439fd50612bbca63a6f55242fa0d2d19c
nivaspuvvada <nivaspuvvada1126@gmail.com>
2023-11-08 14:48:08 -0600

    i/ostreams

---
+++ Demoproject/src/main/java/com/example/Demoproject/iostreams.java
@@ -0,0 +1,36 @@
+package com.example.Demoproject;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class iostreams{
+    public static void main(String[] args) {
+
+        String[] names = {"Nivas", "Sunny", "Puvvada"};
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+            writer.write("Hello everyone.");
+            writer.write("\nHope you all are doing well!!.");
+
+            for (String name : names) {
+                writer.write("\n" + name);
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- Demoproject/src/main/java/com/example/Demoproject/synchronizemt.java
+++ Demoproject/src/main/java/com/example/Demoproject/synchronizemt.java
@@ -8,7 +8,7 @@ class Counter{
     }
 }
 public class synchronizemt {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Counter c = new Counter();
         Thread t1 = new Thread(new Runnable() {
             public void run() {
@@ -25,12 +25,21 @@ public class synchronizemt {
                 }
             }
         });
+        Thread t3 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 1; i <= 8000; i++) {
+                    c.increment();
+                }
+            }
+        });
 
         t1.start();
         t2.start();
+        t3.start();
 
         t1.join();
         t2.join();
+        t3.join();
 
         System.out.println("Count " + c.count);
     }
